65e6b23cd1a32e46566487d1e39ac8dd20f2b5b7
maithani74 <maithanir74@gmail.com>
2025-04-20 06:52:57 +0530

    edits

---
+++ src/app/_not-found/page.tsx
@@ -0,0 +1,9 @@
+// app/not-found.tsx
+export default function NotFound() {
+    return (
+      <div className="text-center p-10">
+        <h1 className="text-2xl font-bold">Page Not Found</h1>
+      </div>
+    );
+  }
+  
